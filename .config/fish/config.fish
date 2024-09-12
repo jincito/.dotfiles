851334e4f094c1f9c000c6d3100c4ade401287dc
jincito <110742524+jincito@users.noreply.github.com>
2024-09-12 18:29:47 -0400

    add ls -Flathr alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias lg='lazygit'
+    alias lsf='ls -Flathr'
 end
+
+# Created by `pipx` on 2024-08-27 19:40:52
+set PATH $PATH /home/cloud/.local/bin
